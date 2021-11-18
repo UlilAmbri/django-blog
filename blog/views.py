27091cb2b03ed6c5af61f332dec831197e44e8d6
Ulil Ambri <ulilambri9888@gmail.com>
2021-11-18 23:02:50 +0700

    Menambah register dan database

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
 def home_view(request):
     context = {}
+
+    post = Post.objects.all()
+    context["posts"] = post
 
     return render(request, "home.html", context)
 
 def about_view(request):
     context = {}
+    context["nama"] = "Ulil Ambri"
+    context["asal"] = "Pekanbaru"
+    context["tinggi"] = "164"
+    context["bb"] = "83"
 
     return render(request, "about.html", context)
